55a03194172c285d75c19bc40378431c9bfe4e80
VargaDot <vancouver_cliparf@simplelogin.com>
2025-01-17 20:25:39 +0100

    Download works

--- pigeon.gd
+++ pigeon.gd
@@ -16,6 +16,7 @@ func _ready() -> void:
 		print("Failed to send request.")
 
 var is_fetching:bool = true
+var download_url:String
 func _on_request_completed(result: int, response_code: int, _headers: PackedStringArray, body: PackedByteArray) -> void:
 	if result == 0 and response_code == 200:
 		if is_fetching:
@@ -25,14 +26,15 @@ func _on_request_completed(result: int, response_code: int, _headers: PackedStri
 			if current_version != latest_version["name"]:
 				print("Update Available! Downloading...")
 
-				var download_url:String = latest_version["assets"][0]["browser_download_url"]
+				download_url = latest_version["assets"][0]["browser_download_url"]
 				print(download_url)
+
+				request(download_url)
+				is_fetching = false
 			else:
 				print("You are using the latest version.")
 		else:
 			print("Download complete, Installing...")
-			var update:Dictionary = JSON.parse_string(body.get_string_from_utf8())
-			
-			print(update)
+			print(body.get_string_from_utf8())
 	else:
 		print("Download failed with result: " + str(result) + " and response code: " + str(response_code))
