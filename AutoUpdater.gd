69e50275441c69663a29dfaf5675461b9185fc00
SP4R0W <sp4r0wxxx@gmail.com>
2025-01-18 09:38:38 +0100

    Revamp of Pigeon; added a UI and simplified the code as well as reorganised it

---
+++ AutoUpdater.gd
@@ -0,0 +1,77 @@
+class_name AutoUpdater extends Node2D
+
+const NEW_PCK_BACKUP_PATH:String = "user://update.pck"
+const OLD_PCK_BACKUP_PATH:String = "user://old.pck"
+
+@export var current_version: String = "v" + ProjectSettings.get_setting("application/config/version")
+@export var pck_url: String = "https://api.github.com/repos/SP4R0W/AutoUpdater/releases/latest"
+
+@onready var fetch_request: HTTPRequest = $FetchRequest
+@onready var download_request: HTTPRequest = $DownloadRequest
+
+@onready var version_text: Label = %VersionText
+@onready var found_text: Label = %Found
+
+var path_to_pck: String
+var download_url: String
+var download_file: String
+
+func _ready() -> void:
+	path_to_pck = OS.get_executable_path().get_base_dir() + "/" + ProjectSettings.get_setting("application/config/name") + ".pck"
+	print(path_to_pck)
+	version_text.text = current_version
+
+func _process(delta: float) -> void:
+	if download_request.get_body_size() > 0:
+		var downloaded: float = float(download_request.get_downloaded_bytes()) / 1024
+		var total: float = float(download_request.get_body_size()) / 1024
+		found_text.text = "Downloaded: " + str(downloaded) + "KB /" + str(total) + "KB"
+
+func _on_fetch_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
+	if result == 0 and response_code == 200:
+		var latest_version: Dictionary = JSON.parse_string(body.get_string_from_utf8())
+
+		if current_version != latest_version["name"]:
+			found_text.text = "Found new version: " + latest_version["name"]
+
+			download_url = latest_version["assets"][0]["browser_download_url"]
+			download_file = "user://" + latest_version["assets"][0]["name"]
+			prints(download_url, download_file)
+		else:
+			found_text.text = "Already has the newest version."
+	else:
+		found_text.text = "Fetch error: " + str(result) + "; " + str(response_code)
+
+func _on_download_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
+	if result == 0 and response_code == 200:
+		set_process(false)
+		found_text.text = "Download complete"
+
+		update_pck()
+
+func _on_check_button_pressed() -> void:
+	var error: Error = fetch_request.request(pck_url)
+
+	if error != OK:
+		found_text.text = "Fetch error: " + str(error)
+
+func _on_download_button_pressed() -> void:
+	download_request.download_file = download_file
+	print(download_request.download_file)
+	var error: Error = download_request.request(download_url)
+
+	if error != OK:
+		found_text.text = "Download error: " + str(error)
+
+func update_pck() -> void:
+	DirAccess.copy_absolute(download_file,path_to_pck)
+
+	found_text.text = "Game was updated. Click the button to restart it."
+
+	$CanvasLayer/UI/Panel/VBoxContainer/Control.hide()
+	$CanvasLayer/UI/Panel/VBoxContainer/Control2.hide()
+	$CanvasLayer/UI/Panel/VBoxContainer/Control3.show()
+
+func _on_restart_button_pressed() -> void:
+	OS.set_restart_on_exit(true)
+	get_tree().quit()
